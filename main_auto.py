4944b4dfb2e1488fca7620610cf4a3875ce86ce5
omidNomiri <omidnomiri@gmail.com>
2024-10-01 19:00:08 +0330

    fix : add and change some line for fix the movement problem

--- main_auto.py
+++ main_auto.py
@@ -4,7 +4,7 @@ from food import  Meat
 
 class Game(arcade.Window):
     def __init__(self):
-        super().__init__(width = 600, height = 600,title = "AI snake game")
+        super().__init__(width = 600, height = 600,title = "Auto snake game")
         arcade.set_background_color(arcade.color.KHAKI)
         self.meat = Meat(self)
         self.snake = Snake(self)
@@ -24,7 +24,7 @@ class Game(arcade.Window):
             self.snake.eat_meat(self.meat)
             self.meat = Meat(self)
 
-        self.snake.move_with_ai(self.meat.center_x, self.meat.center_y)
+        self.snake.move_auto(self.meat.center_x, self.meat.center_y)
         self.snake.move()
 
 if __name__ == "__main__":
--- snake.py
+++ snake.py
@@ -1,56 +1,71 @@
 import arcade
 
+
 class Snake(arcade.Sprite):
     def __init__(self, game):
         super().__init__()
-        self.width = 30
-        self.height = 30
-        self.center_x = game.width // 2
-        self.center_y = game.height // 2
+        self.body = []
+        self.width = 32
+        self.height = 32
+
+        self.center_x = game.width//2
+        self.center_y = game.height//2
         self.change_x = 0
         self.change_y = 0
-        self.body_colors = [arcade.color.WOOD_BROWN, arcade.color.BROWN_NOSE]
+        
+        self.color = arcade.color.BROWN
+        self.colorbody = arcade.color.DEEP_COFFEE
+
         self.speed = 3
-        self.score = 1
-        self.body = []
+        self.score = 0
+
+        self.w = game.width
+        self.h = game.height
 
     def draw(self):
+        arcade.draw_circle_filled(
+            self.center_x, self.center_y, 15, self.color)
+
         for i, part in enumerate(self.body):
-            arcade.draw_circle_filled(part["x"], part["y"], 15, self.body_colors[i % len(self.body_colors)])
-        arcade.draw_circle_filled(self.center_x, self.center_y, 15, arcade.color.DEEP_COFFEE)
+            if i % 2 == 0:
+                self.colorbody = arcade.color.DEEP_COFFEE
+            elif i % 2 == 1:
+                self.colorbody = arcade.color.WOOD_BROWN
+
+            arcade.draw_circle_filled(
+                part['x'], part['y'], 15, self.colorbody)
 
     def move(self):
-        self.body.append({"x": self.center_x, "y": self.center_y})
-        if len(self.body) >= self.score:
-            self.body.pop(0)
+        self.body.append({'x': self.center_x, 'y': self.center_y})
+        if len(self.body) > self.score:
+            for _ in range(len(self.body)-self.score):
+                self.body.pop(0)
+
         self.center_x += self.change_x * self.speed
         self.center_y += self.change_y * self.speed
 
-    def move_with_ai(self, center_x, center_y):
-        if self.center_y > center_y:
-            self.center_y -= self.speed
 
-        if self.center_y < center_y:
-            self.center_y += self.speed
-
-        if self.center_x < center_x:
-            self.center_x += self.speed
-            
-        if self.center_x > center_x:
-            self.center_x -= self.speed
 
     def eat_meatloaf(self, meatloaf):
-        del meatloaf
+        # Handle eating meatloaf
         self.score += 2
+        del meatloaf
 
     def eat_meat(self, meat):
-        del meat
+        # Handle eating meat
         self.score += 1
+        del meat
 
     def eat_plant(self, plant):
-        del plant
+        # Handle eating plant
         self.score -= 1
-        try:
-            self.body.pop(0)
-        except IndexError:
-            arcade.draw_text("GAME OVER!",150,300,arcade.color.RED_DEVIL,35)
+        del plant
+        if not self.body:
+            arcade.draw_text("GAME OVER!", 150, 300, arcade.color.RED_DEVIL, 35)
+            self.score = 0
+
+
+    def game_over(self):
+        arcade.draw_text("GAME OVER!", 150, 300, arcade.color.RED_DEVIL, 35)
+        self.score = 0
+        self.body = []
